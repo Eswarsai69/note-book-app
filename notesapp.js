a726fa102d55e7a6fa309159c4686f2d21b692ae
Eswarsai69 <eswarsaiiyer@gmail.com>
2025-05-06 14:42:15 +0530

    Added all files

---
+++ notesapp.js
@@ -0,0 +1,22 @@
+let createPageBtn=document.querySelector(".create-page-btn");
+let pageContainer=document.querySelector(".pages-container");
+
+
+createPageBtn.addEventListener("click",function(){
+    let page=document.createElement("p");
+    page.className="page";
+    page.setAttribute("contenteditable","true");
+    let dltImg=document.createElement("img");
+    dltImg.src="delete.png";
+    dltImg.className="delete-icon";
+    page.appendChild(dltImg);
+    pageContainer.appendChild(page);
+});
+
+pageContainer.addEventListener("click",function(e){
+    e.preventDefault();
+    if(e.target.tagName==="IMG"){
+        e.target.parentElement.remove();
+    }
+});
+
